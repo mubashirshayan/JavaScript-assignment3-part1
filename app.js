6f32832c58864dfc44fcd6815bf2be0f3b7c3556
mubashirshayan <121729482+mubashirshayan@users.noreply.github.com>
2023-04-19 22:20:46 +0500

    new files

---
+++ app.js
@@ -0,0 +1,128 @@
+//JavaScript Chapter:5 Assignment no3
+//Question no1
+/*var num1=2
+var num2=5
+var total=num1+num2
+document.write('Sum of'+num1+'and'+num2+'is'+total)*/
+//Question no2
+/*var num1=2
+var num2=5
+var total=num1*num2
+var div=num1/num2
+var sub=num1-num2
+var mod=num1%num2
+document.write('Multiplication of'+num1+'and'+num2+'is'+total)
+document.write('division of'+num1+'and'+num2+'is'+' '+div)
+document.write('Subtraction of'+num1+'and'+num2+'is'+' '+sub)
+document.write('Modulus of'+num1+'and'+num2+'is'+' '+mod)*/
+//Question no3
+/*var num;
+document.write(''+num);
+num=5
+document.write('Initial value:'+num+'<br>');
+num++;
+document.write('Value after increament'+num+'<br>');
+num+=7
+document.write('Value after addition'+num+'  <br>');
+num--;
+document.write('Value after increament'+num);
+num%3
+document.write(''+num);*/
+//Question no4
+/*var cost=600
+var quantity=600*5
+document.write('Total cost to buy 5 ticket to a movie is:'+quantity);*/
+//Question no5
+/*var user = +prompt('enter number')
+if (user) {
+  document.write(user + '×' + 1 + '=' + user * 1+'<br>')
+  document.write(user + '×' + 2 + '=' + user * 2 +'<br>')
+  document.write(user + '×' + 3 + '=' + user * 3+'<br>')
+  document.write(user + '×' + 4 + '=' + user * 4 + '<br>')
+  document.write(user + '×' + 5 + '=' + user * 5 + '<br>')
+  document.write(user + '×' + 6+ '=' + user * 6 + '<br>')
+  document.write(user + '×' + 7 + '=' + user * 7 + '<br>')
+  document.write(user + '×' + 8+ '=' + user * 8 + '<br>')
+  document.write(user + '×' + 9+ '=' + user * 9 + '<br>')
+  document.write(user + '×' + 10+ '=' + user * 10 + '<br>')
+}*/
+//Question no6
+/*document.write('<h1>Temperature Converter</h1>')
+var celsuis=35
+var convertF=(celsuis*9/5)+32
+console.log(convertF);
+var fahren=convertF
+var convertC=(fahren-32)*5/9
+console.log(convertC);*/
+//Question no7
+/*document.write('<h1>Shopping Cart</h1>')
+var priItm1=999
+var priItm2=950
+var qtyItm1=2
+var qtyItm2=4
+var charge=200
+var total=(priItm1*qtyItm1)+(priItm2*qtyItm2)+charge;
+document.write('<h1>Shopping Cart</h1>')
+document.write('Price of item 1 is:'+priItm1+'<br>')
+document.write('Quantity of item 1 is:'+qtyItm1+'<br>')
+document.write('Price of item 2 is:'+priItm2+'<br>')
+document.write('Price of item 2 is:'+qtyItm2+'<br>')
+document.write('Shipping Charges:'+charge+'  <br>');
+document.write('Total cost of your order is:'+total);*/
+//Question no8
+/*document.write('<h1>Mark Sheet</h1>')
+var marksTot=980;
+var obtMarks=804;
+var percentage=(marksTot*980)/100;
+document.write('Total Marks:'+marksTot+'<br>')
+document.write('Marks Obtained:'+obtMarks+'<br>')
+document.write('Percentage:'+percentage)
+*/
+//Question no9
+/*document.write('<h1>Currency in Pkr</h1>')
+var _1dollar=280;
+var _1riyal=50;
+var total=(5*_1dollar)+(7*_1riyal);
+document.write('Total Currency in PKR:'+total);*/
+
+//Question no10
+//var num=(12+5*10)/2
+
+
+
+//Question no11
+/*var birthAre=2003;
+var curYear=2023;
+var age=curYear-birthAre;
+document.write('<h1>Age Calculator</h1>')
+document.write('Current Year:'+birthAre+'<br>');
+document.write('Birth Year:'+curYear+'<br>');
+document.write(' Your Age:'+age+'<br>');
+*/
+//Question no 12
+/*var radius=30
+document.write('<h1>The Geometrizer</h1>')
+//formula Circference of a circle =2πr
+var circumference=2*3.142*30
+document.write('The circumference is:'+circumference+'<br>')
+//formula Area of circle=πr2
+var area=(3.142*30)*(3.142*30)
+document.write('The area is:'+area);*/
+//Question no13
+/*document.write('<h1>Lifetime Supply Calculator</h1>')
+var snack='Lays snacks'
+var curAge=19
+var maxAge=65
+var perDay=3
+var total=maxAge-curAge;
+var last=total*3
+document.write('Favorite snack:'+snack+'<br>');
+document.write('Current age:'+curAge+'<br>');
+document.write('Estimated Maximum Age:'+maxAge+'<br>');
+document.write('Amount of snacks per day:'+perDay+'<br>')
+document.write('you will need'+last+snack+'to last you until the ripe old age of'+maxAge);*/
+
+
+
+
+
